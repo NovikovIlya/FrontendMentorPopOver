a3804476679cdc812c1e75e4e8abc21a697ec872
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-03-15 23:25:56 +0300

    Начал верстать

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import { Button, Popover } from 'antd';
 import './App.css';
 
@@ -15,13 +13,21 @@ function App() {
 
   return (
     <>
-      <Popover content={content} title="Title">
-        <Button type="primary">Hover me</Button>
-      </Popover>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <div>
+          Shift the overall look and feel by adding these wonderful
+          touches to furniture in your home
+        </div>
+        <div>Ever been in a room and felt like something was missing? Perhaps
+          it felt slightly bare and uninviting. I’ve got some simple tips
+          to help you make any room feel complete.
+        </div>
+        <div>Michelle Appleton</div>
+        <div>28 Jun 2020</div>
+        <Popover content={content} title="Title">
+          <Button type="primary">Hover me</Button>
+        </Popover>
+      </div>
     </>
   );
 }
